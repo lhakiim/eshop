c295fbcdecefaf63aff47be4c60ca4586425c40f
lhakiim <luqmanulhakim2021@gmail.com>
2025-02-14 19:10:28 +0700

    add functional test

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomeController.java
@@ -0,0 +1,15 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("")
+public class HomeController {
+
+    @GetMapping("")
+    public String getHomePage(){
+        return "HomePage";
+    }
+}
